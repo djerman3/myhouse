fa1997d5d8f549e12562b086734de8cdae0eaaf9
djerman <djerman@pobox.com>
2020-04-25 08:28:36 -0400

    let's get client working for command line debug

--- client/cmd/main.go
+++ client/cmd/main.go
@@ -2,8 +2,9 @@ package main
 
 import "github.com/djerman3/homecontrol/client"
 
+var cfgFilePath := "/home/djerman/projects/homecontrol/etc/homecontrol.json"
 func main() {
-	router_client,err := client.NewClient("[2606:a000:4c84:82f0::1]")
+	router_client,err := client.NewClient(cfgFilePath)
 	if err != nil{
 		log.Fatalf("Bad client:%v\n",err)
 	}
--- client/openwrt.go
+++ client/openwrt.go
@@ -1,26 +1,42 @@
 // Package client sends commands to an openwrt router for uci managment of router stuff
 package client
 
-type LuciRpcAuthRequest struct {
+//LuciRPCMethodRequest allows serialization of the struct to a valid request body
+type LuciRPCMethodRequest struct {
 	ID     int      `json:"id"`
 	Method string   `json:"method"`
 	Params []string `json:"params"`
 }
-type LuciRpcAuthResponse struct {
+
+//LuciRPCSingleResponse allows deserialization of the result body to a valid struct
+// where we expect a simple name:string
+type LuciRPCSingleResponse struct {
 	ID     int    `json:"id"`
 	Result string `json:"result"`
 	Error  string `json:"error"`
 }
 
+//LuciRPCComplexResponse allows deserialization of the result body to a valid struct
+// where we expect a simple name:string
+type LuciRPCComplexResponse struct {
+	ID     int                     `json:"id"`
+	Result *map[string]interface{} `json:"result"`
+	Error  string                  `json:"error"`
+}
+
+//Client remembers the client connection and auth
 type Client struct {
+	cfgFileName   string
 	ServerAddress string
 	BaseURL       string
-	AuthToken     string              `json:"authtoken"`
-	AuthRequest   *LuciRpcAuthRequest `json:ref`
+	AuthToken     string
+	AuthRequest   *LuciRPCMethodRequest // we're going to need this one  alot
 }
 
 //NewClient returns a new client
-func NewClient(address string) (*Client, error) {
-	c := &Client{address, "/cgi-bin/luci/rpc", "", nil}
+func NewClient(cfn string) (*Client, error) {
+	cfgFileName = cfn
+
+	c := &Client{address, "/cgi-bin/luci/RPC", "", nil}
 	return c, nil
 }
